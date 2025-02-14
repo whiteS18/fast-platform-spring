eb3d5d1f3a57ab416017fdb7e3956d7778cc66d7
cwsong <cwsong@88.com>
2025-02-14 18:17:43 +0800

    ORM框架改成Spring Data JPA

--- src/main/java/com/application/next/bean/entity/BaseEntity.java
+++ src/main/java/com/application/next/bean/entity/BaseEntity.java
@@ -1,58 +1,58 @@
 package com.application.next.bean.entity;
 
-import com.mybatisflex.annotation.Column;
-import com.mybatisflex.annotation.Id;
-import com.mybatisflex.annotation.KeyType;
+import jakarta.persistence.*;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.data.annotation.Version;
 
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
+@MappedSuperclass
 public class BaseEntity {
 
 
-    @Id(keyType = KeyType.Auto)
-    @Column(value = "id",comment = "主键id")
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
     private Long id;
 
     @Version
-    @Column(value = "version", onInsertValue = "0",comment = "乐观锁版本号")
+    @Column(name = "version", nullable = false)
     private Integer version;
 
-    @Column(value = "tenant_id", tenantId = true)
-    private String tenantId;
+//    @Column(name = "tenant_id", nullable = false)
+//    private String tenantId;
 
-    @Column(value = "create_id")
+    @Column(name = "create_id")
     private Long createId;
 
-    @Column(value = "create_name")
+    @Column(name = "create_name")
     private String createName;
 
-    @Column(value = "create_time")
+    @Column(name = "create_time", nullable = false)
     private LocalDateTime createTime;
 
-    @Column(value = "update_id")
+    @Column(name = "update_id")
     private Long updateId;
 
-    @Column(value = "update_name")
+    @Column(name = "update_name")
     private String updateName;
 
-    @Column(value = "update_time")
+    @Column(name = "update_time")
     private LocalDateTime updateTime;
 
-    @Column(value = "delete_flag", isLogicDelete = true)
+    @Column(name = "delete_flag", nullable = false)
     private Integer deleteFlag;
 
-    @Column(value = "delete_id")
+    @Column(name = "delete_id")
     private Long deleteId;
 
-    @Column(value = "delete_name")
+    @Column(name = "delete_name")
     private String deleteName;
 
-    @Column(value = "delete_time")
+    @Column(name = "delete_time")
     private LocalDateTime deleteTime;
 
 
